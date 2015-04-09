74a9ca4f0bb7ee3f68bdcf8474c2595debed6e93
Pedro Ribeiro <pedroribeirojr@gmail.com>
2015-04-09 08:06:12 -0400

    Refactoring and final version. Just the score calculation

--- bowling/src/main/scala/bowling.scala
+++ bowling/src/main/scala/bowling.scala
@@ -1,121 +1,43 @@
 package bowling
-class Game(val player:String){
-    var frames = new Frame():: Nil
-        
-    def ball(pins:Int):Game ={
-        def createFrame():Frame=
-            if (frames.size < 10) new Frame()
-            else frames.head.plays match {
-                case Strike()::Nil => new BonusStrike()
-                case _::Spare(x)::Nil => new BonusSpare()
-                case _ => new GameOver()
-            }
 
-        if (frames.head.shallSwitch) 
-            frames = createFrame :: frames
-
-        frames.head.ball(pins)
-        this
+object Bowling { 
+    def score(rolls : Int*):Int = {
+        val frames = turnToFrames(rolls.toList,1)
+        sum(frames)
     }
-    def score():Int = {
-        val inverted = frames.reverse
-        println("score on: " +  inverted.mkString("|"))
-        val plays = frames.foldLeft[List[Ball]](Nil){
-            (result:List[Ball], frame:Frame) => frame.plays.reverse ::: result
-        }
-        println(plays.mkString(","))
-        return doSum(plays)
+    def turnToFrames(rolls: List[Int], rollNum:Int):List[Frame] = rolls match {
+        case 10::tail if (rollNum > 10) => BonusStrike() :: turnToFrames(tail, rollNum + 1)
+        case 10::tail => Strike() :: turnToFrames(tail, rollNum + 1)
+        case x::y::tail if (x + y == 10) => Spare(x, y) :: turnToFrames(tail, rollNum + 1)
+        case x::y::tail => Normal(x, y) :: turnToFrames(tail, rollNum + 1)
+        case Nil => Nil
     }
-    def doSum(plays:List[Ball]):Int = plays match {
-        case Miss()::tail => doSum(tail)
-        case BonusStrikeBall()::tail => 0 + doSum(tail)
-        case Spare(x)::some::tail => 10 + some.pins + doSum(some::tail) 
-        case NormalBall(_)::Spare(x)::tail =>  doSum(Spare(x)::tail)
-        case NormalBall(x)::tail => x + doSum(tail) 
-        case Strike()::some::Nil => 10 + some.pins + doSum(some::Nil)
-        case Strike()::some::other::tail => 10 + some.pins + other.pins + doSum(some::other::tail)
-        case Nil => 0
-    }
-    def gameIsOver():Boolean = (
-        ( frames.size > 10 && !frames.tail.head.spare) 
-    )
-
-    def sumPlays(plays : List[Int]):Int = plays match {
-        case Nil => 0
-        case 10::x::y::Nil => 10 + sumPlays(x::y::Nil)
-        case 10::x::y::tail => 10 + x + y + sumPlays(x::y::tail)
-        case  x::y::z::tail => if (x + y == 10) x + y + z + sumPlays(z::tail) else x + y + sumPlays(z::tail)
-        case  x::tail => x + sumPlays(tail)
-    }
-
-    def frameScore():Int = {
-
-        val score = frames.head.score
-        if (score==10) 0 else score
-    }
-    override def toString():String = {
-        frames.reverse.mkString("|")
-    }
-
+    def sum(frames:List[Frame]):Int = 
+        frames match {
+            case BonusStrike(_, _)::tail=> sum(tail) 
+            case Spare(x, y)::f::tail => x + y + f.x + sum(f::tail)
+            case Strike(_, _)::f::g::tail=> 10 + f.score + g.score + sum(f::g::tail) 
+            case Strike(_, _)::f::tail=> 10 + f.score + sum(f::tail) 
+            case f::tail => f.score + sum(tail)
+            case Nil => 0
+        } 
 }
-sealed abstract class Ball(val pins:Int, strRep:String)
-{
-    override def toString():String = strRep
-}
-case class Strike() extends Ball(10, "X")
-case class Spare (override val pins:Int) extends Ball(pins, "/")
-case class Miss() extends Ball(0,"-")
-case class NormalBall (override val pins:Int) extends Ball(pins, pins.toString)
-case class BonusSpareBall (override val pins:Int) extends Ball(pins, pins.toString)
-case class BonusStrikeBall () extends Ball(10, "X")
-
-class GameOver extends Frame {
-    override def shallSwitch():Boolean=false 
-    override def ball(pins:Int) = 
-        throw new IllegalStateException("Game is over")
-}
-class BonusSpare extends Frame {
-    override def shallSwitch():Boolean=false
-    override def ball(pins:Int) = {
-        if (plays.size > 1) 
-            throw new IllegalStateException("Game is over")
-        plays = BonusSpareBall(pins)::plays
-    }
+trait Frame {
+    val score:Int
+    val x:Int
+    val y:Int
 }
 
-class BonusStrike extends Frame {
-    override def shallSwitch():Boolean=false
-    override def ball(pins:Int) = {
-        if (plays.size > 1) 
-            throw new IllegalStateException("Game is over")
-        plays = BonusStrikeBall()::plays
-    }
+case class Normal(override val x:Int ,override val y:Int) extends Frame {
+    override val score = x + y
+}
+case class Spare(override val x:Int ,override val y:Int) extends Frame {
+    override val score = x + y
 }
 
-class Frame(){
-    var plays: List[Ball]=Nil
-
-    def score():Int = plays.foldLeft(0){
-        _ + _.pins
-    }
-    def ball(pins:Int) {
-        if (pins == 10 && plays.isEmpty)
-            plays = Strike()::Nil
-        else if (pins ==0)
-            plays = Miss()::plays
-        else if (plays.nonEmpty && plays.head.pins + pins == 10)
-            plays = Spare(pins)::plays
-        else 
-            plays = NormalBall(pins)::plays
-    }
-
-    def shallSwitch():Boolean=
-        (plays.size == 2) || (score == 10)
-
-
-    def strike():Boolean = false
-    def spare():Boolean = false
-    override def toString():String={
-        plays.reverse.mkString
-    }
+case class Strike(override val x:Int=10, override val y:Int=0) extends Frame{
+    override val score = 10
+}
+case class BonusStrike(override val x:Int=10, override val y:Int=0) extends Frame {
+    override val score = 10
 }
--- bowling/src/test/scala/bowling-spec.scala
+++ bowling/src/test/scala/bowling-spec.scala
@@ -1,105 +1,39 @@
-import bowling._
+import bowling.Bowling._
 import org.scalatest.FunSuite
 import org.scalatest.BeforeAndAfter
+import org.scalatest.ShouldMatchers
 
-class BowlingSpec extends FunSuite with BeforeAndAfter {
+class BowlingSpec extends FunSuite with BeforeAndAfter with ShouldMatchers{
 
-    var game : Game = _
-    before {
-        game = new Game("John")
+    test("The Score of two simple rolls will be their sum"){
+        score(2, 3) should be (5)
+        score(3, 4, 5, 1) should be ( 7 + 6 )
     }
-
-    test("A frame score will be the sum of the tries"){
-
-        game.ball(3)
-        game.ball(3)
-        assert(game.frameScore == 6)
+    test("Spares should add the next roll"){
+       score(4, 6, 6, 1) should be ( (4+ 6 + 6) + 6 + 1) 
     }
-    test("A frame score will be zero if a strike happens"){
-        game.ball(10)
-        assert(game.frameScore==0)
-
+    test("Strikes should add following rolls "){
+        score(10, 5, 2) should be ( 10 + 7 + 7)
     }
-    test("A frame score will be zero if a spare happens"){
-        game.ball(1)
-        game.ball(9)
-        assert(game.frameScore==0)
+    test("Game X | 5 / = (10 + 5 + 5) + 5 + 5)"){
+        score(10, 5, 5) should be (10 + 10 + 10)
     }
-    test("A strike should be 'X'"){
-        game.ball(10)
-        assert(game.toString == "X")
-    }
-    test("A spare should be the try plus / "){
-        game.ball(1)
-        game.ball(9)
-        assert(game.toString == "1/")
-    }
-    test("A simple play should display both tries together"){
-        assert(game.ball(3).ball(4).toString == "34")
-    }
-    test("misses should be displayed with - "){
-        assert(game.ball(0).ball(4).toString == "-4")
-        assert(game.ball(3).ball(0).toString == "-4|3-")
-    }
-    test("If a try hasnt happened yet, display nothing"){
-        assert(game.toString == "")
-        assert(game.ball(0).toString == "-")
-    }
-    test("A set of frames should be separated by '|'"){
-        game.ball(2)
-        game.ball(3)
-        game.ball(4)
-        game.ball(6)
-        game.ball(10)
-        assert(game.toString == "23|4/|X")
-    }
-    test("A total score is the sum of the plays if they are simple"){
-        game.ball(2)
-        game.ball(7)
-        assert(game.score == 9)
-    }
-    test("Play 1/|3: Score 10 + 3 + 3"){
-        game.ball(1)
-        game.ball(9)
-        game.ball(3)
-        assert(10 + 3 + 3 == game.score)
-    }
-    test("Play: X|1: Score 10 + 1 + 0  + 1 "){
-        game.ball(10).ball(1) 
-        assert(game.score == 10 + 1  + 1 )
-
-    }
-    test("Play: X|12: Score 10 + 1 + 2  + 1 + 2 "){
-        game.ball(10) ball(1) ball(2)
-        assert(game.score == 10 + 1  + 2 + 3 )
-    }
-    test("Play: X|X|3 is 10 + 13 + 10 + 3 + 3"){
-        game.ball(10) ball(10) ball(3)
-
-        assert(game.score == (10 + 10 + 3) + (10 + 3 ) + 3)
-    }
-    test("Perfect game 30 x 10"){
-        game  ball(10) ball(10) ball(10) ball(10) ball(10) ball(10) ball(10) ball(10) ball(10) ball(10) ball(10) ball(10)
-        assert(game.score == 300)
+    test("A Spare in the end gives you a bonus balls"){
+        score( 0, 0,    0, 0,   0, 0,   0, 0,   0, 0,
+               0, 0,    0, 0,   0, 0,   0, 0,   7,3, 10 ) should be ( (10 + 10) )
     }
 
-/*    test("The Game ends at the tenth play"){
-        ( game ball(10) ball(10) ball(10) ball(10) ball(10) 
-               ball(10) ball(10) ball(10) ball(10) ball(3) ball(1) )
-
-        intercept [IllegalStateException] {
-            game.ball(1)
-        }
+    test("A Strike in the end gives you two bonus balls"){
+        score( 0, 0,    0, 0,   0, 0,   0, 0,   0, 0,
+               0, 0,    0, 0,   0, 0,   0, 0,   10, 10, 10) should be ( 10 + 10 + 10 )
+    }
+    test("Game X | X | X "){
+        score (10, 10, 10) should be (30 + 20 + 10)
+    }
+    test("A perfect game "){
+        score( 10, 10, 10, 10, 10, 
+               10, 10, 10, 10, 10, 10, 10) should be (300)
 
     }
-    test("The game gives a bonus play if the 10th is a spare "){
-        ( game ball(10) ball(10) ball(10) ball(10) ball(10) 
-               ball(10) ball(10) ball(10) ball(10) ball(3) ball(7) )
-        game ball(2)
-        intercept [IllegalStateException] {
-            game.ball(1)
-        }
 
-    }
-*/
 }
